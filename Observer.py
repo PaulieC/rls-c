da15024bfcd8efa3048135b1c53d892a7077f82f
pbelko <belkop43@students.rowan.edu>
2014-11-18 11:36:54 -0500

    added comments

--- Observer.py
+++ Observer.py
@@ -1,5 +1,9 @@
 __author__ = 'Pat and Tony'
 
+#abstract class for the Observer interface
 class Observer(object):
+
+    #replaces the normal notify method
+    #must be implemented in all subclasses
     def launchICBM(self, msg):
         pass
