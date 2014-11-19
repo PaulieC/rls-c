1558001995bccac92c99d6acc2ac23047816a72d
Joe Kvedaras <jokvedaras@gmail.com>
2014-11-18 23:04:11 -0500

    Created registration class and added register method to add a player to the tournament

---
+++ registration.py
@@ -0,0 +1,14 @@
+__author__ = 'joe kvedaras'
+#purpose of this file is to register a player in a tournament
+
+
+class Registration:
+    """Register a player in the tournament"""
+
+    def __init__(self,Tournament):
+        self.tournament = Tournament
+
+
+    def register(self, Player):
+        """Register a player in the tournament"""
+        self.tournament.add(Player)
