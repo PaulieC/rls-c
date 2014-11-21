76e8c07e41cba460ab67091dd3d276557b827a3a
geebzter <geebzter@gmail.com>
2014-11-20 18:57:51 -0800

    Updated Game class interface

--- Game.py
+++ Game.py
@@ -1,13 +1,29 @@
 #################################################
 # 
-# Game designed to work with RPSFramework
+# Game specification for the RPSFramework
+#
 # Created by Matt Martorana + Justin Read
 #
 #################################################
 
-class Game ():
-    
-       def get_result(self, moves):
+import Observer
+
+class Game (Observer.Observer):
+
+
+    def num_players_per_game(self):
+        """
+        Number of players in a game. Default setting is two-player games
+        :return:
+        """
+        return 2
+
+    def get_result(self, moves):
+        """
+        Computes the result for the given moves.
+        :param moves: A tuple containing the moves made by the players
+        :return: a tuple containing the result for the players
+        """
         # child of this class will have to figure out how win/loss/tie
         # is determined moves
         #
@@ -15,3 +31,12 @@ class Game ():
         # 
         # @return array of points given to each player
         pass
+
+    def is_legal(self, move):
+        """
+        Checks if a given move is legal
+        :param move: given move
+        :return: True if the move is legal, false otherwise
+        """
+        pass
+
