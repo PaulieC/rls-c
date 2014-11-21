af521c5199559c97a8aa34ad197ec0021576b9f7
Overblaze <overblaze36@yahoo.com>
2014-11-20 19:49:51 -0500

    Simplified Game Class extremely

--- Game.py
+++ Game.py
@@ -5,38 +5,13 @@
 #
 #################################################
 
-class Game (Object):
-    listPlayers = []
-
-    listRules = []
-
-    listMoves = []
-
-    # Need to get list of Players involved
-    # All we need is to pass in a list of players involved and
-    # a list for their moves respectively
-    def get_players(self, playerList, moveList):
-        listPlayers = playerList
-        listMoves = moveList
-
-    # Need a list of valid moves, most likely way will be
-    # ['rock>scissors','scissors>paper','paper>rock']
-    # or other simple boolean statements
-    #
-    def get_rules(self, ruleList):
-        listRules = ruleList
-
-
-    # need method to determine winners
-    def determine_winner(self, moves):
-        # child of this class will have to figure out how winners
-        # are determined using the listRules, listMoves
-        #
-        # Also take note, eval('string') evaluates string as if it were
-        # a boolean or mathematic expression, and exec('string')
-        # executes the string as if it were exact python code.
+class Game ():
+    
+       def get_result(self, moves):
+        # child of this class will have to figure out how win/loss/tie
+        # is determined moves
         #
         # Don't forget an elimination process if move is illegal
         # 
-        # @return String array of winner(s) and move is preferable
+        # @return array of points given to each player
         pass
