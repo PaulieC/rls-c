d94500b5f120c797c09bf9a86507fc95710c0f49
Joe Kvedaras <jokvedaras@gmail.com>
2014-11-23 21:35:56 -0500

    Created TournamentService with register_player(player) and set_game(game)

---
+++ TournamentService.py
@@ -0,0 +1,22 @@
+__author__ = "Joe Kvedaras and Collin Day"
+#Set up a tournament with a game and register players
+from Tournament.py import *
+
+
+class TournamentService:
+
+    def __init__(self, tournament):
+        """initialize a new tournament with a game"""
+        self.tournament = tournament
+
+
+    def register_player(self, player):
+        """register a player in the current tournament"""
+        self.tournament.register_player(player)
+
+
+    def set_game(self,game):
+        """set the game of the current tournament"""
+        #Do not know if I have to create a new tournament with the game
+        #type or add the game to an existing tournament
+        pass
