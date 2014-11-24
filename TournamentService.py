a04dcef729e2baf1e9fbfcb3a081a08efafa5008
Joe Kvedaras <jokvedaras@gmail.com>
2014-11-24 11:53:28 -0500

    Added set_tournament and run methods

--- TournamentService.py
+++ TournamentService.py
@@ -1,13 +1,14 @@
 __author__ = "Joe Kvedaras and Collin Day"
 #Set up a tournament with a game and register players
 from Tournament.py import *
-
+from AllPlayAll.py import *
 
 class TournamentService:
 
-    def __init__(self, tournament):
+    def __init__(self):
         #Is tournament service the one to create a new tournament?
-        self.tournament = tournament
+        self.tournament
+        self.game
 
 
     def register_player(self, player):
@@ -15,8 +16,25 @@ class TournamentService:
         self.tournament.register_player(player)
 
 
-    def set_game(self,game):
+    def set_game(self, game):
         """set the game of the current tournament"""
-        #Do not know if I have to create a new tournament with the game
-        #type or add the game to an existing tournament
-        pass
+        #Tournament initializes with a game and you can not change
+        #game type afterwards
+        self.game = game
+
+    def set_tournament(self, tournament_type = None):
+        """Allow client to set the tournament type. Defaults to
+        AllPlayAll tournament type"""
+        if tournament_type is None:
+            #AllPlayAll takes registration as a parameter. I don't think they
+            #need that there
+            self.tournament = AllPlayAll(self.game, None, 1000)
+        else:
+            self.tournament = tournament_type(self.game, None, 1000)
+
+    def run(self):
+        """Set the game and run the tournament"""
+        if self.tournament is None:
+            print ("Can not run tournament. Tournament is null")
+        else:
+            self.tournament.run()
