fa291ea10cc558ade7bf6b61246d510b9948d704
Greg <richar45@students.rowan.edu>
2014-11-24 18:55:49 -0500

    Update RPSGame.py
    updated indentation and documentation

--- RPSGame.py
+++ RPSGame.py
@@ -5,28 +5,32 @@ import Game
 
 class RPSGame(Game):
     ## this class simulates two players playing a game of rock, paper, scissors
-    def __init__(self):
-        super(RPSGame, self).__init__()
+        def __init__(self):
+            super(RPSGame, self).__init__()
 
-        def get_result(self, moves):
-            move1, move2 = moves    # unpack the tuple that was passed as a parameter
-            x = is_legal(move1)
-            y = is_legal(move2)
-            result = (0,0)          # result is a tuple of the points that each player has earned respectively 
-            if x and y:
-                if move1 == move2:
-                    result = (1,1)
-                elif (move1 == 0 and move2 != 1) or (move1 == 1 and move2 != 2) or (move1 == 2 and move2 != 0):
-                    result = (1,0)
+            def get_result(moves):
+                # unpack the tuple that was passed as a parameter
+                move1, move2 = moves
+                x = is_legal(move1)
+                y = is_legal(move2)
+                result = (0, 0)
+                if x and y:
+                    if move1 == move2:
+                        result = (1, 1)
+                    elif (move1 == 0 and move2 != 1) \
+                            or (move1 == 1 and move2 != 2) \
+                            or (move1 == 2 and move2 != 0):
+                        # result is tuple with points each player has earned respectively
+                        result = (1, 0)
+                    else:
+                        result = (0, 1)
+                elif x and not y:
+                    result = (1, 0)
+                elif not x and y:
+                    result = (0, 1)
                 else:
-                    result = (0,1)
-            elif x and not y:
-                result = (1,0)
-            elif not x and y:
-                result = (0,1)
-            else:
-                result = result
-            return result
+                    result = result
+                return result
 
-        def is_legal(self, move):
-            return move in (0, 1, 2)
+            def is_legal(move):
+                return move in (0, 1, 2)
