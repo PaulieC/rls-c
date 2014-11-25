2fa8e8ad68d2737aeedbadf9947805e02f5a1b13
geebzter <geebzter@gmail.com>
2014-11-25 02:01:22 -0800

    Minor fixes

--- RPSDriver.py
+++ RPSDriver.py
@@ -1,31 +1,19 @@
 __author__ = 'Tony'
 
 from TournamentService import *
-from AllPlayAll import *
 from RPSPlayerExample import *
 from RPSGame import *
 
 # Create a tournament service that will set the tournament accordingly, register the players to the tournament
 # and run the instance of Tournament
-class RPSDriver(TournamentService.TournamentService):
+class RPSDriver(TournamentService):
 
     def __init__(self):
-        self.tournament
-        self.game
-
-    def set_tournament(self, tournament_type):
-        pass
-
-    def register_player(self, player):
-        pass
+        TournamentService.__init__(self)
 
     def register_players(self, player_list):
         for player in player_list:
-            self.register_player(self,player)
-
-    def set_game(self, game):
-        pass
-
+            self.register_player(player)
 
 if __name__ == "__main__":
     driver = RPSDriver()
@@ -35,8 +23,7 @@ if __name__ == "__main__":
     rps = RPSGame()
     driver.register_players(players)
     driver.set_game(rps)
-    driver.set_tournament(AllPlayAll)
-    driver.tournament.run()
+    driver.run()
 
 
 
