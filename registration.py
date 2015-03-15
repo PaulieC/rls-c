24a7d46ee41461fccf23489ae760d522dc33d3fe
Paul Council <counci70@students.rowan.edu>
2015-03-15 16:17:01 -0400

    sprint1_Council_b -- refactored code from Python3 to Python2. Added headers and cleaned code annotations. Corrected some code logic.

--- registration.py
+++ registration.py
@@ -1,13 +1,28 @@
-__author__ = 'Joe Kvedaras and Collin Day'
-#purpose of this file is to register a player in a tournament
+__author__ = "Paul Council, Joseph Gonzoph, Anand Patel"
+__version__ = "sprint1"
+__credits__ = ["Joe Kvedaras, Collin Day"]
 
 
 class Registration:
+    """
+    purpose of this file is to register a player in a tournament
+
+    Attributes:
+        tournament: the tournament to register players to
+    """
 
     def __init__(self, tournament):
-        """Initialize Registration with a Tournament"""
+        """
+        Initialize Registration with a Tournament
+        :param tournament: the tournament for registration
+        :type tournament: TournamentService.TournamentService
+        """
         self.tournament = tournament
 
     def register(self, player):
-        """Register a player in the tournament"""
-        self.tournament.add(player)
+        """
+        Register a player in the tournament
+        :param player: the player to register to the current tournament
+        :type player: Player.Player
+        """
+        self.tournament.register_player(player)
